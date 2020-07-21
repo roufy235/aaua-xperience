fa27da54598f3b50359e11bbdcbe37f1d3f416b3
Abdulrouf <roufy235@gmail.com>
2020-07-21 11:19:05 +0100

    Project Setup

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>
+      Hello World
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!--suppress SassScssResolvedByNameOnly -->
+<style scoped lang="scss">
+h2 {
+  color: $green;
+}
+</style>
