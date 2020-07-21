ac0e7f1f04208ddc5f264661f2d4d2fa98caf7cb
Abdulrouf <roufy235@gmail.com>
2020-07-21 15:10:32 +0100

    Nave implemented

---
+++ components/indexComponents/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="banner">
+    <div class="overlayBanner">
+      <h2 class="text-center">
+        Explore on-going and upcoming
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+}
+</script>
+
+<style scoped lang="scss">
+h2 {
+  color: white;
+  font-weight: 700;
+}
+.overlayBanner {
+  background-color: $bannerOverlay;
+  height: $bannerHeight;
+}
+.banner {
+  background-image: url('/images/banner.jpeg');
+  background-size: cover;
+  height: $bannerHeight;
+  background-position: bottom;
+  background-repeat: no-repeat;
+}
+</style>
