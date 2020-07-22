a9cad86b3cd0b9d8dbc92c2964e31b1fbbf417ff
Abdulrouf <roufy235@gmail.com>
2020-07-22 21:18:24 +0100

    added more features

--- components/indexComponents/HowItWorks.vue
+++ components/indexComponents/HowItWorks.vue
@@ -1,7 +1,62 @@
 <template>
   <section>
-    <div class="gifImage">
-      <div class="overlayBanner"></div>
+    <div class="call_section">
+      <div class="wrapper">
+        <div class="container margin_80_55">
+          <div class="text-center">
+            <h2>How it Works</h2>
+          </div>
+          <div class="row works">
+            <div class="col-md-4">
+              <div class="box_how">
+                <i class="pe-7s-search"></i>
+                <h3>Search Locations</h3>
+                <p class="text-center">
+                  An nec placerat repudiare scripserit, temporibus complectitur
+                  at sea, vel ignota fierent eloquentiam id.
+                </p>
+                <span></span>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="box_how">
+                <i class="pe-7s-info"></i>
+                <h3>View Location Info</h3>
+                <p class="text-center">
+                  An nec placerat repudiare scripserit, temporibus complectitur
+                  at sea, vel ignota fierent eloquentiam id.
+                </p>
+                <span></span>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="box_how">
+                <i class="pe-7s-like2"></i>
+                <h3>Book, Reach or Call</h3>
+                <p class="text-center">
+                  An nec placerat repudiare scripserit, temporibus complectitur
+                  at sea, vel ignota fierent eloquentiam id.
+                </p>
+              </div>
+            </div>
+          </div>
+          <!-- /row -->
+          <b-row>
+            <b-col></b-col>
+            <b-col>
+              <nuxt-link
+                to="/register"
+                class="btn-block btn btn-danger reg ripple-parent"
+                data-v-56cc9c65=""
+              >
+                Sign Up
+              </nuxt-link>
+            </b-col>
+            <b-col></b-col>
+          </b-row>
+        </div>
+      </div>
+      <!-- /wrapper -->
     </div>
   </section>
 </template>
@@ -14,15 +69,32 @@ export default {
 
 <!--suppress SassScssResolvedByNameOnly -->
 <style scoped lang="scss">
-.gifImage {
-  background-image: url('/gif/how_it_works.gif');
-  height: $bannerHeight;
-  background-repeat: no-repeat;
+.call_section {
+  background-image: url('/images/bannerTwo.jpg');
   background-size: cover;
-  background-position: center;
+  background-position: bottom;
+  background-repeat: no-repeat;
 }
-.overlayBanner {
-  background-color: $bannerOverlay;
-  height: $bannerHeight;
+.works {
+  margin-bottom: 30px;
+}
+p {
+  color: white;
+}
+h2 {
+  color: white;
+  font-weight: 500 !important;
+  margin-bottom: 50px;
+}
+h3 {
+  color: white;
+  font-weight: 500 !important;
+  text-align: center;
+}
+.call_section .wrapper {
+  background-color: #000000e8;
+  position: relative;
+  z-index: 22;
+  padding: 60px 0 60px 0;
 }
 </style>
